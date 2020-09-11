14b3e789017f41f307820b2509e1d0db2ba0eeae
cdubles <33189090+cdubles@users.noreply.github.com>
2020-09-11 00:58:48 -0400

    Fix POC

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import pyttsx3
 import os
 import speech_recognition as sr
-from time import ctime
+import time
 engine = pyttsx3.init()
 
-def say(string):
-    engine.say(string)
-    engine.runAndWait()
+while True:
+    def say(string):
+        engine.say(string)
+        engine.runAndWait()
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    say("Say something!")
-    audio = r.listen(source)
-    data = r.recognize_google(audio)
-    print(data)
-if "time" in data:
-    say(ctime())
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        say("Say something!")
+        audio = r.listen(source)
+        data = r.recognize_google(audio)
+        print(data)
+    if "time" in data:
+        say(time.strftime('%c'))
