84ed01a8649305855eec72e7b6038bb7c2a60f84
cdubles <33189090+cdubles@users.noreply.github.com>
2021-02-10 12:57:14 -0500

    start time module

---
+++ coreModules/timeModule.py
@@ -0,0 +1,10 @@
+import time
+
+def what_time():
+    return(time.strftime("%H:%M"))
+
+def what_year():
+    return(time.strftime("%Y"))
+
+def what_date():
+    return time.strftime("%D")
--- main.py
+++ main.py
@@ -2,18 +2,31 @@ import pyttsx3
 import os
 import speech_recognition as sr
 import time
+import  coreModules.timeModule as timeMod
 engine = pyttsx3.init('sapi5')
+r = sr.Recognizer()
 
 def say(string):
         engine.say(string)
         engine.runAndWait()
 
 while True:
-    r = sr.Recognizer()
     with sr.Microphone() as source:
         say("Say something!")
-        audio = r.listen(source)
-        data = r.recognize_google(audio)
+        audio = r.listen(source,timeout=5)
+        print(audio)
+        try:
+            data = r.recognize_google(audio)
+        except:
+            say('bad audio')
+            continue
         print(data)
-    if "time" in data:
-        say(time.strftime('%c'))
+        if "time" in data:
+            say(timeMod.what_time())
+        if 'year' in data:
+            say(timeMod.what_year())
+        if 'date' in data:
+            say(timeMod.what_date())
+        if 'close' in data:
+            say("good bye")
+            break
